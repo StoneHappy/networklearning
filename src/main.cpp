c168888ce4f70e43e496c3d206c40e79acde7383
xhguleixin123 <18017158729@163.com>
2021-10-13 16:09:13 +0800

    ✨ feat: 修改习题

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include"ch01/fd_seri.h"
 #include"ch01/tcp_client.h"
 #include"ch01/ex_server.h"
+#include"ch01/ex_client.h"
 #include<iostream>
 int main()
 {
@@ -17,6 +18,7 @@ int main()
     std::cout << "105、第一章 fd_seri" << std::endl;
     std::cout << "106、第一章 tcp_client" << std::endl;
     std::cout << "107、第一章 ex_server" << std::endl;
+    std::cout << "108、第一章 ex_client" << std::endl;
     std::cin >> flag;
     char** tmp;
     switch (flag)
@@ -47,6 +49,11 @@ int main()
     case 107:
         *(tmp+1) = "5400";
         glx0107::main(2, tmp);
+        break;
+    case 108:
+        *(tmp+1) = "127.0.0.1";
+        *(tmp+2) = "5400";
+        glx0108::main(3, tmp);
         break;
     default:
         break;
